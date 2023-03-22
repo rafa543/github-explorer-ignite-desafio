5af743d17a752c169b7c83a16debe9a3877036b7
Rafael Araujo <rafa123839@gmail.com>
2023-03-22 19:41:16 -0300

    first commit

---
+++ src/components/Card/CardAnimation/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { useWindowDimensions, ViewProps } from 'react-native';
+import {
+  Extrapolate,
+  interpolate,
+  runOnJS,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from 'react-native-reanimated';
+
+import { AnimationContainer } from './styles';
+
+interface CardAnimationProps extends ViewProps {
+  children: React.ReactNode;
+}
+
+export function CardAnimation({ children, ...rest }: CardAnimationProps) {
+  const { width: displayWidth } = useWindowDimensions();
+  const cardOpacity = useSharedValue(0);
+  const cardOffset = useSharedValue(0.25 * displayWidth);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: cardOpacity.value,
+      transform: [
+        {
+          translateX: cardOffset.value
+        }
+      ]
+    }
+  })
+
+  useEffect(() => {
+    cardOpacity.value = withTiming(
+      1,
+      { duration: 1000 },
+      () => {
+        'worklet'
+      }
+    )
+    cardOffset.value = withTiming(
+      0,
+      { duration: 1000 },
+      () => {
+        'worklet'
+      }
+    )
+  }, []);
+
+  return (
+    <AnimationContainer {...rest} style={animatedStyle}>
+      {children}
+    </AnimationContainer>
+  )
+}
